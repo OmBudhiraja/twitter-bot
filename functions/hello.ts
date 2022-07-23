a3c6489b527f9a3838c623df3af61af4480ed0de
Om Budhiraja <ombudhiraja144@gmail.com>
2022-07-23 23:11:30 +0530

    moving up the functions folder in root

--- functions/hello.ts
+++ functions/hello.ts
@@ -1,4 +1,6 @@
-export const handler = async (event: any) => {
+import type { Handler } from '@netlify/functions';
+
+export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
